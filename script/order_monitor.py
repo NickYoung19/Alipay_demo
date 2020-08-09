a8220bcba2f42182b08d79d719c4af969c23ea74
Nick <pengyang19@dingtalk.com>
2020-08-09 16:47:52 +0800

    过期订单监听

---
+++ script/order_monitor.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+# @Author      : Nick
+# @File        : order_monitor.py  v1.0
+# @Contact     : PENGYANG19@163.COM
+# @CreateTime  : 2020-08-09 14:30:11
+# Copyright (C) 2020 Nick Ltd. All rights reserved.
+import json
+import time
+
+import requests
+from utils.common import Redis
+
+# 创建Redis对象并连接redis数据库
+redis = Redis().connection()
+# 创建pub_sub对象，该对象订阅一个频道并侦听新消息：
+pub_sub = redis.pubsub()
+
+
+# 定义触发事件
+def event_handler(msg):
+    # {'type': 'pmessage', 'pattern': '__keyevent@0__:expired', 'channel': '__keyevent@0__:expired', 'data': 'nick_1'}
+    print('捕获到订阅消息：', msg)
+    data = msg.get('data')  # 获取键，创建订单设置SETEX key seconds value
+    data_list = data.split('_')
+    if data_list[0] == 'nick':  # 在创建订单，写入redis key时加上prefix前缀：nick_
+        order_id = data_list[-1]
+        print(f'过期的订单id：{order_id}')
+        url = 'http://192.168.0.106:8008/payment/cancel_order/'
+        result = requests.post(url=url, json=dict(order_id=order_id))
+        data = json.loads(result.content.decode("UTF-8"))
+        print("事件执行返回状态：", data)
+
+
+# 订阅redis键空间通知
+pub_sub.psubscribe(**{'__keyevent@0__:expired': event_handler})
+
+
+def main():
+    # 主方法，死循环，不停的接收订阅的通知
+    while True:
+        message = pub_sub.get_message()
+        if message:
+            # {'type': 'psubscribe', 'pattern': None, 'channel': '__keyevent@0__:expired', 'data': 1}
+            print(f"订阅的消息: {message}")
+            print('正在监听过期订单。。。。。。')
+            time.sleep(0.5)
+        else:
+            time.sleep(0.5)
+
+
+if __name__ == '__main__':
+    main()
--- utils/common.py
+++ utils/common.py
@@ -1,4 +1,20 @@
 # -*- coding: utf-8 -*-
+import json
+import os
+
+import redis
+
+
+# 项目环境配置信息
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+CONFIG_INFO = {}
+try:
+    CONFIG_FILE = BASE_DIR + '/.env'
+    CONFIG_INFO = open(CONFIG_FILE).read()
+    CONFIG_INFO = json.loads(CONFIG_INFO)
+except Exception as E:
+    print(E)
+    print("没有找到项目env文件...")
 
 
 def get_domain(request):
@@ -7,3 +23,26 @@ def get_domain(request):
     domain = request.scheme + "://" + request.META['HTTP_HOST']
 
     return domain
+
+
+class Redis(object):
+    def __init__(self):
+        self.r = None
+        self.host = CONFIG_INFO.get("REDIS_HOST")
+        self.port = CONFIG_INFO.get("REDIS_PORT")
+        self.password = CONFIG_INFO.get("REDIS_PASSWORD")
+        self.db = CONFIG_INFO.get("REDIS_DB")
+
+    def connection(self):
+        """连接 redis"""
+        # decode_responses 将获取到的字节码转为字符串；
+        # socket_connect_timeout 设置连接超时时间为 1s；
+        # socket_time 设置连接建立后的读写操作超时为 1s；
+        self.r = redis.StrictRedis(host=self.host,
+                                   port=self.port,
+                                   password=self.password,
+                                   db=self.db,
+                                   decode_responses=True,
+                                   socket_connect_timeout=1,
+                                   socket_timeout=1)
+        return self.r
